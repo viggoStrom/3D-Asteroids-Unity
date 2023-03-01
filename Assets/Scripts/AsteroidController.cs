86980ee6a28562be88b29abc77683dded38772ff
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2023-03-01 10:45:28 +0100

    added abhays asteroids

--- Assets/Scripts/AsteroidController.cs
+++ Assets/Scripts/AsteroidController.cs
@@ -4,15 +4,113 @@ using UnityEngine;
 
 public class AsteroidController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float mass;
+    public float maxLifeTime;
+
+    public float collisonDistanceOffset;
+
+    public float minScale;
+    public float maxScale;
+
+    public float minSpeed;
+    public float maxSpeed;
+
+    public float rotationValue;
+    public float rotationMagnifier;
+
+    private Rigidbody rigidBody;
+
+    Transform asteroidTransform;
+    Vector3 randomRotation;
+
+    private Vector3 randomMovement(float min, float max)
     {
-        
+        var x = Random.Range(min, max);
+        var y = Random.Range(min, max);
+        var z = Random.Range(min, max);
+        return new Vector3(x, y, z);
     }
 
-    // Update is called once per frame
-    void Update()
+    private Vector3 randomScale(float min, float max)
     {
-        
+        var x = Random.Range(min, max);
+        var y = Random.Range(min, max);
+        var z = Random.Range(min, max);
+        return new Vector3(x, y, z);
     }
+
+    private float randomMass(float max)
+    {   
+            var m = Random.Range(1, max);
+            return m;
+    }
+
+
+    private void CreateSplit()
+    {
+        Vector3 explosionPosition = this.asteroidTransform.position;
+        explosionPosition = explosionPosition + Random.insideUnitSphere * collisonDistanceOffset;
+        AsteroidController half = Instantiate(this, explosionPosition, this.asteroidTransform.rotation);
+
+        half.asteroidTransform.localScale = new Vector3((this.asteroidTransform.localScale.x * 0.5f), (this.asteroidTransform.localScale.y * 0.5f), (this.asteroidTransform.localScale.y * 0.5f));
+        half.mass = this.mass * 0.5f;
+        half.GetComponent<Rigidbody>().mass = half.mass;
+        half.GetComponent<Rigidbody>().velocity = randomMovement(minSpeed, maxSpeed);
+
+        this.asteroidTransform.localScale = new Vector3((this.asteroidTransform.localScale.x * 0.5f), (this.asteroidTransform.localScale.y * 0.5f), (this.asteroidTransform.localScale.y * 0.5f));
+        this.mass *= 0.5f;
+        this.GetComponent<Rigidbody>().mass = this.mass;
+        this.GetComponent<Rigidbody>().velocity = randomMovement(minSpeed, maxSpeed);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Asteroids")
+        {
+            if ((this.mass * 0.5f) >= 1)
+            {
+                CreateSplit();
+            }
+            else 
+            {
+                Destroy(this.gameObject);
+            }  
+        }
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Asteroids")
+    //    {
+    //     Destroy(this.gameObject);  
+    //    }
+    //}
+
+    private void Awake()
+    {
+        asteroidTransform = transform;
+    }
+
+    private void Start()
+    {
+        //Instantiates a Random rotation for the gameObject
+        randomRotation.x = Random.Range(-rotationValue, rotationValue);
+        randomRotation.y = Random.Range(-rotationValue, rotationValue);
+        randomRotation.z = Random.Range(-rotationValue, rotationValue);
+
+
+        asteroidTransform.localScale = randomScale(minScale, maxScale);
+        var rigidBody = GetComponent<Rigidbody>();
+        rigidBody.mass = randomMass(mass);
+        rigidBody.velocity = randomMovement(minSpeed, maxSpeed);
+        Destroy(this.gameObject, maxLifeTime);
+
+    }
+
+    private void Update()
+    {
+        asteroidTransform.Rotate(randomRotation * Time.deltaTime * rotationMagnifier);  
+    }
+
 }
---
+++ Assets/Scripts/AsteroidsParentSpawn.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidsParentSpawn : MonoBehaviour
+{
+    public GameObject asteroidPrefab;
+    private float spawnRate;
+    Vector3 randomRotation;
+
+    public float spawnDelay;
+    public float spawnDistanceOffset;
+    public float rotationValue;
+
+
+    void Update()
+    {
+        transform.Rotate(randomRotation * Time.deltaTime);
+        if (shouldSpawn())
+        {
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        spawnRate = Time.time + spawnDelay;
+        Vector3 spawnDirection = Random.insideUnitSphere.normalized * this.spawnDistanceOffset;
+        Vector3 spawnPoint = this.transform.position + spawnDirection;
+        Quaternion rotation = Quaternion.AngleAxis(Random.Range(-this.rotationValue, this.rotationValue), Vector3.left);
+
+        Instantiate(this.asteroidPrefab, spawnPoint, rotation);
+    } 
+
+    private bool shouldSpawn()
+    {
+        return Time.time > spawnRate;
+    }
+
+    void Start()
+    {
+        randomRotation.x = Random.Range(-rotationValue, rotationValue);
+        randomRotation.y = Random.Range(-rotationValue, rotationValue);
+        randomRotation.z = Random.Range(-rotationValue, rotationValue);
+    }
+
+}
